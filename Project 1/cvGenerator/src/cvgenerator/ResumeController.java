3f46bb88572f0d4aabfed3f3f9866b9d9c6a538e
Shadman S. Khan <shadmankhan95@gmail.com>
2019-07-28 03:19:29 +0600

    Made a dropdown box for primary education selector

--- Project 1/cvGenerator/src/cvgenerator/ResumeController.java
+++ Project 1/cvGenerator/src/cvgenerator/ResumeController.java
@@ -15,10 +15,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
@@ -31,6 +34,8 @@ import org.jsoup.nodes.Document;
  * @author shadm
  */
 public class ResumeController implements Initializable {
+    
+    ObservableList<String> primaryEducationList = FXCollections.observableArrayList("SSC", "O-levels");
     
     private Label label;
     @FXML
@@ -57,6 +62,8 @@ public class ResumeController implements Initializable {
     private TextField ImageInput;
     @FXML
     private Button ImageUpload;
+    @FXML
+    private ChoiceBox PrimaryChooser;
  
     
     
@@ -64,8 +71,17 @@ public class ResumeController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+       
     }    
 
+    @FXML
+    private void initialize(){
+        
+        PrimaryChooser.setValue("SSC");
+        PrimaryChooser.setItems(primaryEducationList);
+        
+    }
+    
     @FXML
     private void SubmitResume(ActionEvent event) {
         
