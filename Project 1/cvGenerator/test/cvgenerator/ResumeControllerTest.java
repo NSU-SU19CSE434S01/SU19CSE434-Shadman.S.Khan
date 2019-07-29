dab358efed79a2225faebfc9e331986b152f9c87
Shadman S. Khan <shadmankhan95@gmail.com>
2019-07-30 03:37:08 +0600

    Made new test class

---
+++ Project 1/cvGenerator/test/cvgenerator/ResumeControllerTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cvgenerator;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author shadm
+ */
+public class ResumeControllerTest {
+    
+    public ResumeControllerTest() {
+    }
+
+    /**
+     * Test of initialize method, of class ResumeController.
+     */
+    @Test
+    public void testInitialize() {
+    }
+    
+}
