c827600114f09c9ca28415265bb25af2b71de7f0
Shadman <shadman.khan@northsouth.edu>
2019-09-04 11:22:56 +0600

    refactored to create seperate testing methods

--- Project 2/Selenium Project/src/selenium/project/SeleniumProject.java
+++ Project 2/Selenium Project/src/selenium/project/SeleniumProject.java
@@ -10,17 +10,26 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumProject {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    WebDriver driver;
+    
+    public void InvokeBrowser(){
         
-        WebDriver driver;
         System.setProperty("webdriver.gecko.driver", "F:\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.get("http://phptravels.com/demo/");
         
         driver.findElement(By.xpath("//a[@class='btn btn-primary btn-lg btn-block' and 1]")).click();
+        
+    }
+    
+    
+    public static void main(String[] args) {
+        
+     SeleniumProject testOpen = new SeleniumProject();
+     testOpen.InvokeBrowser();
+        
+        
+        
         
     }
     
