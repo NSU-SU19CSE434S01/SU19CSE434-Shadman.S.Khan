ebb7a56f9a4545a514e1167035b357b0acecbf79
Shadman <shadman.khan@northsouth.edu>
2019-09-04 11:51:11 +0600

    implemented loginTest

--- Project 2/Selenium Project/src/selenium/project/SeleniumProject.java
+++ Project 2/Selenium Project/src/selenium/project/SeleniumProject.java
@@ -24,7 +24,17 @@ public class SeleniumProject {
         
         driver = new FirefoxDriver();
         driver.get("https://www.phptravels.net/");
-        
+        driver.findElement(By.xpath("//ul/ul[@class='nav navbar-nav navbar-side navbar-right sidebar go-left user_menu' and 1]/li[@id='li_myaccount' and 1]")).click();
+        driver.findElement(By.xpath("//li[@class='open']/ul[@class='dropdown-menu' and 1]/li[1]/a[@class='go-text-right' and 1]")).click();
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("user1@phptravels.com");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("demouser");
+        driver.findElement(By.xpath("//button[@class='btn btn-action btn-lg btn-block loginbtn']")).click();
+        if("https://www.phptravels.net/account/".equals(driver.getCurrentUrl()))
+        {
+            System.out.println("login Successful");
+        }
+        else
+            System.out.println(driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText());
     }
     
     
