531fe0724638ca6f91b84e7ef3c6183df7911b55
Shadman <shadman.khan@northsouth.edu>
2019-09-04 12:12:49 +0600

    initialized SignUpTest

--- Project 2/Selenium Project/src/selenium/project/SeleniumProject.java
+++ Project 2/Selenium Project/src/selenium/project/SeleniumProject.java
@@ -26,7 +26,7 @@ public class SeleniumProject {
         driver.get("https://www.phptravels.net/");
         driver.findElement(By.xpath("//ul/ul[@class='nav navbar-nav navbar-side navbar-right sidebar go-left user_menu' and 1]/li[@id='li_myaccount' and 1]")).click();
         driver.findElement(By.xpath("//li[@class='open']/ul[@class='dropdown-menu' and 1]/li[1]/a[@class='go-text-right' and 1]")).click();
-        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("user1@phptravels.com");
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("user@phptravels.com");
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys("demouser");
         driver.findElement(By.xpath("//button[@class='btn btn-action btn-lg btn-block loginbtn']")).click();
         if("https://www.phptravels.net/account/".equals(driver.getCurrentUrl()))
@@ -37,13 +37,33 @@ public class SeleniumProject {
             System.out.println(driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText());
     }
     
+    public void signUpTest(){
+        
+        driver = new FirefoxDriver();
+        driver.get("https://www.phptravels.net/register");
+        driver.findElement(By.xpath("//div[3]/input[@class='form-control' and 1]")).sendKeys("Shadman");
+        driver.findElement(By.xpath("//div[4]/input[@class='form-control' and 1]")).sendKeys("S. Khan");
+        driver.findElement(By.xpath("//div[5]/input[@class='form-control' and 1]")).sendKeys("017000000");
+        driver.findElement(By.xpath("//div[6]/input[@class='form-control' and 1]")).sendKeys("shadman@gmail.com");
+        driver.findElement(By.xpath("//div[7]/input[@class='form-control' and 1]")).sendKeys("12345");
+        driver.findElement(By.xpath("//div[8]/input[@class='form-control' and 1]")).sendKeys("123456");
+        driver.findElement(By.xpath("//button[@class='signupbtn btn_full btn btn-action btn-block btn-lg']")).click();
+        
+        if("https://www.phptravels.net/account/".equals(driver.getCurrentUrl()))
+            System.out.println("SignUp Successful");
+        else
+        {
+            System.out.println(driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText());
+        }
+    }
+    
     
     public static void main(String[] args) {
         
         
      System.setProperty("webdriver.gecko.driver", "F:\\geckodriver.exe");   
      SeleniumProject testOpen = new SeleniumProject();
-     testOpen.testLogin();
+     testOpen.signUpTest();
         
         
         
