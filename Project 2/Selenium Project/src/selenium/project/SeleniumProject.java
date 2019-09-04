39e39f64bb26bc83dcdeac0ebf36f470562aac87
Shadman <shadman.khan@northsouth.edu>
2019-09-04 13:01:46 +0600

    initialized bookingtest

--- Project 2/Selenium Project/src/selenium/project/SeleniumProject.java
+++ Project 2/Selenium Project/src/selenium/project/SeleniumProject.java
@@ -57,6 +57,12 @@ public class SeleniumProject {
         }
     }
     
+    public void bookingTest(){
+        
+        
+        
+    }
+    
     
     public static void main(String[] args) {
         
